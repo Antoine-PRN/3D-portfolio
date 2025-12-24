5c86eb91654952e38a452510d21c1e00cbfa8d50
Antoine-PRN <titounet.perrin@gmail.com>
2025-12-24 13:39:07 +0100

    fix typo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { arrow } from "../assets/icons";
-import CTA from "../components/cta";
+import CTA from "../components/CTA";
 import { projects } from "../constants";
 import { Link } from 'react-router-dom'
 const Projects = () => {
