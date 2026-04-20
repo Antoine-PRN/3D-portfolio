8c49c59933e78fbede80fc8ac4eb9a403d5b3713
Antoine-PRN <titounet.perrin@gmail.com>
2026-04-20 14:44:19 +0200

    Ajout de Matomo

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './index.css'
+import { MatomoProvider, createInstance } from '@matomo-org/tracker-react'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const instance = createInstance({
+    urlBase: 'https://matomo.antoine-perrin.fr',
+    siteId: 1,
+})
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <MatomoProvider value={instance}>
+        <App />
+    </MatomoProvider>
+)
